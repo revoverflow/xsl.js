7652d73ab69ca4d625a74ecb78e7be8aa234deb7
Thomas POYETON <poyetont@gmail.com>
2023-04-03 20:34:55 +0200

    Adding Network Timing attacks support

--- xsl.js
+++ xsl.js
@@ -68,3 +68,137 @@ XSL._probeErrorPromise = function (options) {
         });
     });
 };
+
+/*
+    * XSL.fetchTimed
+    * @description: Fetches a resource and returns the time it took to fetch it.
+    * @param {Object} options
+    * @param {String} options.url
+    * @param {String} options.method
+    * @param {Object} options.headers
+    * @param {String} options.body
+    * @param {String} options.mode
+    * @param {String} options.cache
+    * @param {Function} options.callback
+    * @return {Promise}
+*/
+XSL.fetchTimed = function (options) {
+    options = options || {};
+
+    if (!options.url) {
+        throw new Error('url is required');
+    }
+
+    if (!options.callback) {
+        return XSL._fetchTimedPromise(options);
+    }
+
+    let fetchOptions = {
+        method: options.method || 'GET',
+        headers: options.headers || {},
+        body: options.body || null,
+        mode: options.mode || 'no-cors',
+        cache: options.cache || 'default'
+    };
+
+    let start = performance.now();
+
+    fetch(options.url, fetchOptions)
+        .then(function (response) {
+            if (response.ok) {
+                return response.text();
+            } else {
+                throw new Error('fetch failed');
+            }
+        })
+        .then(function (text) {
+            let end = performance.now();
+            options.callback(end - start, text);
+        })
+        .catch(function (err) {
+            let end = performance.now();
+            options.callback(end - start, err);
+        });
+};
+
+/*
+    * XSL._fetchTimedPromise
+    * @description: Promise wrapper for XSL.fetchTimed
+    * @param {Object} options
+    * @param {String} options.url
+    * @param {String} options.method
+    * @param {Object} options.headers
+    * @param {String} options.body
+    * @param {String} options.mode
+    * @param {String} options.cache
+    * @return {Promise}
+*/
+XSL._fetchTimedPromise = function (options) {
+    return new Promise(function (resolve, reject) {
+        XSL.fetchTimed({
+            url: options.url,
+            method: options.method,
+            headers: options.headers,
+            body: options.body,
+            mode: options.mode,
+            cache: options.cache,
+            callback: function (time, data) {
+                resolve({
+                    time: time,
+                    data: data
+                });
+            }
+        });
+    });
+};
+
+/*
+    * XSL.imageTimed
+    * @description: Loads an image and returns the time it took to load it.
+    * @param {Object} options
+    * @param {String} options.url
+    * @param {Function} options.callback
+    * @return {Promise}
+*/
+XSL.imageTimed = function (options) {
+    options = options || {};
+
+    if (!options.url) {
+        throw new Error('url is required');
+    }
+
+    if (!options.callback) {
+        return XSL._imageTimedPromise(options);
+    }
+
+    let start = performance.now();
+
+    let img = new Image();
+    img.src = options.url;
+    img.onload = function () {
+        let end = performance.now();
+        options.callback(end - start);
+    };
+    img.onerror = function () {
+        let end = performance.now();
+        options.callback(end - start);
+    };
+}
+
+/*
+    * XSL._imageTimedPromise
+    * @description: Promise wrapper for XSL.imageTimed
+    * @param {Object} options
+    * @param {String} options.url
+    * @return {Promise}
+*/
+XSL._imageTimedPromise = function (options) {
+    return new Promise(function (resolve, reject) {
+        XSL.imageTimed({
+            url: options.url,
+            callback: function (time) {
+                resolve(time);
+            }
+        });
+    });
+}
